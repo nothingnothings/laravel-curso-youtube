8328939dab6f08dcafc8cb128d44cd60b71006a6
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2024-12-03 17:53:42 -0300

    finished lesson 6

--- projeto/example_project/resources/views/welcome.blade.php
+++ projeto/example_project/resources/views/welcome.blade.php
@@ -1,21 +1,4 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Laravel</title>
-
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
-    <!-- Styles / Scripts -->
-    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-    @else
-    @endif
-</head>
+@extends('layout')
 <body>
     <h1>Algum título</h1>
     @if($nome === "Arthur") {
@@ -42,13 +25,14 @@
         echo $name2;
 
         @endphp
-{{-- COMENTÁRIO DO BLADE, QUE NÃO APARECE NO HTML FINAL --}}
+        {{-- COMENTÁRIO DO BLADE, QUE NÃO APARECE NO HTML FINAL --}}
 
 
         @foreach($someNames as $name)
         <p>{{ $loop->index }}/p>
-        <p>{{ $name }}</p>
-        @endforeach
+            <p>{{ $name }}</p>
+            @endforeach
 
+            <img src="/img/banner.jpg" alt="Laravel">
 </body>
 </html>
