426a2cd6afe2eaff1ee8407bf04d010e2f3acca9
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2024-12-04 13:34:48 -0300

    finished lesson 24

--- projeto/example_project/app/Http/Controllers/EventController.php
+++ projeto/example_project/app/Http/Controllers/EventController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Event;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -88,7 +89,9 @@ class EventController extends Controller
 
         $event = Event::findOrFail($eventId);
 
-        return view('events.show', ['event' => $event]);
+        $eventOwner = User::where('id', $event->user_id)->first()->toArray();
+
+        return view('events.show', ['event' => $event, 'eventOwner' => $eventOwner]);
     }
 
     /**
--- projeto/example_project/resources/views/events/show.blade.php
+++ projeto/example_project/resources/views/events/show.blade.php
@@ -19,7 +19,7 @@
                 <ion-icon name="people-outline"></ion-icon> X Participantes
             </p>
             <p class="event-owner">
-                <ion-icon name="star-outline"></ion-icon> Dono do Evento:
+                <ion-icon name="star-outline"></ion-icon> {{ $eventOwner['name'] }}
             </p>
             <a href="#" class="btn btn-primary" id="event-submit">Confirmar Presença</a>
             <h3>O evento conta com:</h3>
